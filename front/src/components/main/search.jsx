3483e4ea5a2062e5b9e0cc35c6fd2157792f6463
Triloksingh2804 <122295623+Triloksingh2804@users.noreply.github.com>
2024-04-14 00:00:05 +0530

    redrect

--- front/src/components/main/search.jsx
+++ front/src/components/main/search.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import axios from '../../axios';
 import Cookies from 'js-cookie';
+import { useNavigate } from "react-router-dom";
 const defaultpicture =
   "https://www.pngarts.com/files/10/Default-Profile-Picture-PNG-Download-Image.png";
 const UserProfileSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const accessToken = Cookies.get('accessToken');
+  const navigate = useNavigate();
 
   const handleChange = async (e) => {
     const { value } = e.target;
@@ -29,6 +31,11 @@ const UserProfileSearch = () => {
       setSearchResults([]);
     }
   };
+  const clickedProfile=(username)=>{
+    navigate(`/profile/${username}`);
+  }
+
+ 
 
   return (
   <div className='flex w-full justify-center'>
@@ -40,21 +47,22 @@ const UserProfileSearch = () => {
       onChange={handleChange}
       className="w-full px-4 py-2 border border-gray-300 rounded-md mb-4"
     />
-    <ul className="text-white">
+    <ul className="text-white -ml-2">
       {searchResults.map((profile) => (
-        <li key={profile.username} className="flex items-center mb-4">
+        <li key={profile.username} className="flex items-center mb-4 " onClick={()=>clickedProfile(profile.username)} style={{ cursor: 'pointer' }}>
 
          <img
             src={profile.avatar}
             alt={profile.username}
             className="w-8 h-8 rounded-full mr-2 object-cover"
+            style={{width:'4rem', height:'4rem'}}
             onError={(e) => { e.target.src = defaultpicture; }}
             
           />
 
-          <div className="bg-white rounded-md p-2">
-            <p className="text-black font-semibold">Username: {profile.username}</p>
-            <p className="text-gray-700">Full Name: {profile.fullname}</p>
+          <div className="bg-white rounded-md p-2 ">
+            <p className="text-black font-semibold">{profile.username}</p>
+            <p className="text-gray-700"> {profile.fullname}</p>
           </div>
         </li>
       ))}
