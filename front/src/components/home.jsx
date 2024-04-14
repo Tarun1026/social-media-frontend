99b186d4c16361feb7039ca537d5b90de96f595e
Triloksingh2804 <122295623+Triloksingh2804@users.noreply.github.com>
2024-04-14 17:51:16 +0530

    icons

--- front/src/components/home.jsx
+++ front/src/components/home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
+import { MdHome, MdSearch, MdPerson, MdCreate,MdNotificationsNone,MdOutlineMessage,MdOutlineExplore } from "react-icons/md";
+import { BsCameraReels } from "react-icons/bs";
 export default function Home() {
   let username;
   const accessToken = Cookies.get("accessToken");
@@ -25,26 +27,34 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col bg-black h-screen">
-        <div className="font-bold text-4xl text-indigo-600 px-4">Snap Sphere</div>
+        <div className="font-bold text-4xl text-indigo-600 px-4 hidden sm:block">Snap Sphere</div>
         <div className="text-white px-4">
           <ul className="py-12">
             <li className="py-3 font-bold  text-2xl" onClick={gotohome}
-              >Home</li>
+              ><div className="flex flex-row"><MdHome className="md:hidden"/>
+              <div className="hidden md:flex">Home</div></div></li>
             <li className="py-3 font-bold text-2xl" onClick={gotosearch
             }
-            >Search</li>
-            <li className="py-3 font-bold text-2xl">Reels</li>
-            <li className="py-3 font-bold text-2xl">Notifications</li>
-            <li className="py-3 font-bold text-2xl">Messages</li>
-            <li className="py-3 font-bold text-2xl" onClick={gotoprofile}>Profile</li>
-            <li className="py-3 font-bold text-2xl">Explore</li>
-            <li className="py-3 font-bold text-2xl" onClick={gotopost}>Create</li>
+            ><div className="flex flex-row"><MdSearch className="md:hidden"/>
+            <div className="hidden md:flex">Serach</div></div></li>
+            <li className="py-3 font-bold text-2xl"><div className="flex flex-row"><BsCameraReels className="md:hidden"/>
+              <div className="hidden md:flex">Reels</div></div></li>
+            <li className="py-3 font-bold text-2xl"><div className="flex flex-row"><MdNotificationsNone className="md:hidden"/>
+              <div className="hidden md:flex">Notification</div></div></li>
+            <li className="py-3 font-bold text-2xl"><div className="flex flex-row"><MdOutlineMessage className="md:hidden"/>
+              <div className="hidden md:flex">Messages</div></div></li>
+            <li className="py-3 font-bold text-2xl" onClick={gotoprofile}><div className="flex flex-row"><MdPerson className="md:hidden"/>
+              <div className="hidden md:flex">Profile</div></div></li>
+            <li className="py-3 font-bold text-2xl"><div className="flex flex-row"><MdOutlineExplore className="md:hidden"/>
+              <div className="hidden md:flex">Explore</div></div></li>
+            <li className="py-3 font-bold text-2xl" onClick={gotopost}><div className="flex flex-row"><MdCreate className="md:hidden"/>
+              <div className="hidden md:flex">Create</div></div></li>
             {/* <li className="py-10 font-bold text-2xl">Settings</li> */}
             {/* <li className="bottom-0  font-bold text-2xl">More</li> */}
           </ul>
         </div>
-        <div className="text-white   p-4 font-bold  text-2xl ">More</div>
-        <div className="text-white   ml-4 font-bold  text-2xl">Threads</div>
+        <div className="text-white   p-4 font-bold  text-2xl hidden sm:block ">More</div>
+        <div className="text-white   ml-4 font-bold  text-2xl hidden sm:block">Threads</div>
         {/* <div className="text-red-400 bottom-0 p-4 font-bold  text-2xl">More</div> */}
       </div>
     </>
